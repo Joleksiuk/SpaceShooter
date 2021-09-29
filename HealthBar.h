3528dc57ff6fa9083d21d8b74fa5ff6dc43ea77c
Julia <julkaoleksiuk26@gmai.com>
2021-09-29 10:26:12 +0200

    Fixed HealthBar header

--- HealthBar.h
+++ HealthBar.h
@@ -1,1 +1,22 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+class HealthBar
+{
+	sf::Sprite sprite;
+	sf::RectangleShape shape;
+
+	void initSprite(sf::Texture* texture, float pox_x, float pos_y);
+	void initShape(float pox_x, float pos_y);
+
+
+public:
+
+	float hp;
+
+	HealthBar(sf::Texture* texture, float pox_x, float pos_y, int hp);
+	virtual ~HealthBar();
+
+	void update();
+	void render(sf::RenderTarget& target);
+};
+
